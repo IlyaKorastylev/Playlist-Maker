7ce572253bde28bd7ad6ee927234e086288a008d
Илья Корастылев <i.korastylev@yandex.ru>
2023-02-19 21:14:07 +0300

    В SettingsActivity.kt добавлены обработчики нажатий на кнопки Поделиться, Поддержка, и Пользовательское соглашение.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,37 @@ class SettingsActivity : AppCompatActivity() {
             val searchBackIntent = Intent(this, MainActivity::class.java)
             startActivity(searchBackIntent)
             finish()
+        }
+
+        val shareButton = findViewById<Button>(R.id.share_button)
+
+        shareButton.setOnClickListener {
+            val message = "https://practicum.yandex.ru/android-developer/"
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.data = Uri.parse("text/plain")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            val chooserIntent = Intent.createChooser(shareIntent,null)
+            startActivity(chooserIntent)
+        }
+
+        val supportButton = findViewById<Button>(R.id.support_button)
+
+        supportButton.setOnClickListener {
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("i.korastylev@yandex.ru"))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            supportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(supportIntent)
+        }
+
+        val agreementButton = findViewById<Button>(R.id.agreement_button)
+
+        agreementButton.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW,Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(agreementIntent)
         }
     }
 }
